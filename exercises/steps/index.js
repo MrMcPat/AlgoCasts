1b1afd52608616b1d0b961634cdaddd7f0c4257a
Patrick Liang <pl204511194@yahoo.com>
2022-04-26 19:43:18 -0400

    solved section 11

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    let step = " ".repeat(n)
+    let stepArr = step.split("")
+    for(let i = 0; i < n; i++) {
+        stepArr[i] = "#"
+        console.log(stepArr.join(""))
+    }
+}
+
+//Recursive solution
+// function steps(n, row = 0, stair = "") {
+//     if (n === row) {
+//         return;
+//     }
+
+//     if (n === stair.length) {
+//         console.log(stair)
+//         return steps(n, row + 1)
+//     }
+
+//     if (stair.length <= row) {
+//         stair += "#"
+//     } else {
+//         stair += " "
+//     }
+//     steps(n, row, stair)
+// }
 
 module.exports = steps;

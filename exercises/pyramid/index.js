e108a93cf49da548963432516826f5da2f374620
Patrick Liang <pl204511194@yahoo.com>
2022-04-26 20:18:04 -0400

    solved section 12

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,16 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    let step = " ".repeat(n + n-1)
+    let stepArr = step.split("")
+    for(let i = 0; i < n; i++) {
+        stepArr[i + Math.floor((n+n-1)/2)] = "#"
+        stepArr[Math.floor((n+n-1)/2) - i] = "#"
+        console.log(stepArr.join(""))
+    }
+}
+
+pyramid(5)
 
 module.exports = pyramid;

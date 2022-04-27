5d90377b07adc3e8f6bd40fcfcee77a69e657625
Patrick Liang <pl204511194@yahoo.com>
2022-04-26 20:48:37 -0400

    pyramid recursive solution :(

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -24,6 +24,28 @@ function pyramid(n) {
     }
 }
 
-pyramid(5)
+//Recursive solution
+// function pyramid(n, row = 0, level = "") {
+//     if (row === n) {
+//         return;
+//     }
+    
+//     if (level.length === n + n - 1) {
+//         console.log(level)
+//         return pyramid(n, row + 1)
+//     }
+
+//     const midpoint = Math.floor((n + n - 1) / 2)
+//     let add;
+
+//     if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//         add = "#"
+//     } else {
+//         add = " "
+//     }
+//     pyramid(n, row, level + add)
+// }
+
+pyramid(3)
 
 module.exports = pyramid;

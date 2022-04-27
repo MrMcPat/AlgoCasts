019f5ed746fc4e52db3a6f63a3c5fecea8ff38ea
Patrick Liang <pl204511194@yahoo.com>
2022-04-27 12:30:40 -0400

    completed section 16

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    let array = [0, 1]
+
+    if (n === 0) {
+        return 0
+    } else {
+        for (let i = 2; i <= n; i++) {
+            array.push(array[i - 1] + array[i - 2])
+        }  
+        return array.pop()
+    }
+}
+
+//Memoization + Recursion
+// function memoize(fn) {
+//     const cache = {}
+//     return function(...args) {
+//         if (cache[args]) {
+//             return cache[args]
+//         }
+
+//         const result = fn.apply(this, args)
+//         cache[args] = result
+
+//         return result
+//     }
+// }
+
+// function slowFib(n) {
+//     if (n < 2) {
+//         return n
+//     }
+
+//     return fib(n - 1) + fib (n - 2)
+// }
+
+// const fib = memoize(slowFib)
 
 module.exports = fib;
